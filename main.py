0725589e60532cb3b7af5969e7efbc036b487d9d
werble7 <bravehenrique@gmail.com>
2023-01-30 11:50:12 -0300

    Manutenção do menu principal, o último teste resultava em muitos erros

--- main.py
+++ main.py
@@ -1,187 +1,185 @@
-import _mysql_connector
 import mysql.connector
+
 
 def createTables():
 
-    criarTabelas = """
-    drop table if exists lesaojogador;
-    drop table if exists jogador;
-    drop table if exists clube;
-    drop table if exists estadio;
-    drop table if exists tecnico;
-    drop table if exists lesao;
-    drop table if exists jogo;
-    drop table if exists competicao;
-    drop table if exists cartaojogador;
-    drop table if exists cartaotecnico;
-    drop table if exists titulo;
-    drop table if exists arbitro;
-    
-    create table arbitro (
-        idArbitro int unsigned auto_increment,
-        nome varchar(80) not null,
-        dataNasc date not null,
-        qtdJogos int not null,
-        dataEstreia date not null,
-        primary key (idArbitro));
-    
-    create table estadio (
-        idEstadio int unsigned auto_increment,
-        nome varchar(80) not null,
-        dataInaug date not null,
-        valor float not null,
-        capacidade int not null,
-        primary key(idEstadio));
-    
-    create table lesao (
-        idLesao int unsigned auto_increment,
-        descricao varchar(150) not null,
-        primary key (idLesao));
-    
-    create table competicao (
-        idComp int unsigned auto_increment,
-        descricao varchar(150) not null,
-        primary key (idComp));
-    
-    create table tecnico (
-        idTecnico int unsigned auto_increment,
-        nome varchar(80) not null,
-        dataNasc date not null,
-        qtdJogos int not null,
-        dataEstreia date not null,
-        primary key (idTecnico));
-    
-    create table clube (
-        idClube int unsigned auto_increment,
-        nome varchar(80) not null,
-        dataFund date not null,
-        idEstadio int unsigned,
-        idTecnico int unsigned not null,
-        primary key (idClube),
-        foreign key (idEstadio) references estadio (idEstadio),
-        foreign key (idTecnico) references tecnico (idTecnico));
-    
-    create table jogador (
-        idJogador int unsigned auto_increment,
-        nome varchar(80) not null,
-        dataNasc date not null,
-        posicao varchar(20) not null,
-        qtdJogos int not null,
-        qtdGols int not null,
-        qtdAssist int not null,
-        idClube int unsigned,
-        primary key (idJogador),
-        foreign key (idClube) references clube (idClube));
-    
-    create table LesaoJogador (
-        idLesao int unsigned not null,
-        idJogador int unsigned not null,
-        dataLesao date not null,
-        gravidade varchar(20) not null,
-        regiao varchar(20) not null,
-        dataRecu date,
-        primary key (idLesao, idJogador),
-        foreign key (idLesao) references lesao (idLesao),
-        foreign key (idJogador) references jogador (idJogador));
-    
-    create table jogo (
-        idJogo int unsigned not null,
-        idClube1 int unsigned not null,
-        idClube2 int unsigned not null,
-        idArbitro int unsigned not null,
-        resultado varchar(10),
-        idComp int unsigned not null,
-        primary key (idJogo),
-        foreign key (idClube1) references clube (idClube),
-        foreign key (idClube2) references clube (idClube),
-        foreign key (idArbitro) references arbitro (idArbitro),
-        foreign key (idComp) references competicao (idComp));
-    
-    create table CartaoJogador (
-        idCartao int unsigned auto_increment,
-        idJogador int unsigned not null,
-        idJogo int unsigned not null,
-        idArbitro int unsigned not null,
-        cor varchar(20) not null,
-        primary key (idCartao),
-        foreign key (idJogador) references jogador (idJogador),
-        foreign key (idJogo) references jogo (idJogo),
-        foreign key (idArbitro) references arbitro (idArbitro));
-    
-    create table CartaoTecnico (
-        idCartao int unsigned auto_increment,
-        idTecnico int unsigned not null,
-        idJogo int unsigned not null,
-        idArbitro int unsigned not null,
-        cor varchar(20) not null,
-        primary key (idCartao),
-        foreign key (idTecnico) references Tecnico (idTecnico),
-        foreign key (idJogo) references jogo (idJogo),
-        foreign key (idArbitro) references arbitro (idArbitro));
-    
-    create table titulo (
-        idTitulo int unsigned auto_increment,
-        idComp int unsigned not null,
-        idClube int unsigned not null,
-        ano int not null,
-        idArtilheiro int unsigned not null,
-        primary key (idTitulo),
-        foreign key (idComp) references competicao (idComp),
-        foreign key (idClube) references clube (idClube),
-        foreign key (idArtilheiro) references jogador (idJogador));
-    """
+    arbitro = """create table if not exists arbitro (
+	idArbitro int unsigned auto_increment,
+	nome varchar(80) not null,
+	dataNasc date not null,
+	qtdJogos int not null,
+	dataEstreia date not null,
+	primary key (idArbitro)); """
 
-    try:
-        cursor.execute(criarTabelas)
-        connection.commit()
-    except _mysql_connector.MySQLInterfaceError:
-        pass
+    cursor.execute(arbitro)
+    connection.commit()
 
-    # end code
+    estadio = """create table if not exists estadio (
+	idEstadio int unsigned auto_increment,
+	nome varchar(80) not null,
+	dataInaug date not null,
+	valor float not null,
+	capacidade int not null,
+	primary key(idEstadio));"""
+
+    cursor.execute(estadio)
+    connection.commit()
+
+    lesao = """create table if not exists lesao (
+	idLesao int unsigned auto_increment,
+	descricao varchar(150) not null,
+	primary key (idLesao));"""
+
+    cursor.execute(lesao)
+    connection.commit()
+
+    comp = """create table if not exists competicao (
+	idComp int unsigned auto_increment,
+	descricao varchar(150) not null,
+	primary key (idComp));"""
+
+    cursor.execute(comp)
+    connection.commit()
+
+    tecnico = """create table if not exists tecnico (
+	idTecnico int unsigned auto_increment,
+	nome varchar(80) not null,
+	dataNasc date not null,
+	qtdJogos int not null,
+	dataEstreia date not null,
+	primary key (idTecnico));"""
+
+    cursor.execute(tecnico)
+    connection.commit()
+
+    clube = """create table if not exists clube (
+	idClube int unsigned auto_increment,
+	nome varchar(80) not null,
+	dataFund date not null,
+	idEstadio int unsigned,
+	idTecnico int unsigned not null,
+	primary key (idClube),
+	foreign key (idEstadio) references estadio (idEstadio),
+	foreign key (idTecnico) references tecnico (idTecnico));"""
+
+    cursor.execute(clube)
+    connection.commit()
+
+    jogador = """create table if not exists jogador (
+	idJogador int unsigned auto_increment,
+	nome varchar(80) not null,
+	dataNasc date not null,
+	posicao varchar(20) not null,
+	qtdJogos int not null,
+	qtdGols int not null,
+	qtdAssist int not null,
+	idClube int unsigned,
+	primary key (idJogador),
+	foreign key (idClube) references clube (idClube));"""
+
+    cursor.execute(jogador)
+    connection.commit()
+
+    lesaojog = """create table if not exists LesaoJogador (
+	idLesao int unsigned not null,
+	idJogador int unsigned not null,
+	dataLesao date not null,
+	gravidade varchar(20) not null,
+	regiao varchar(20) not null,
+	dataRecu date,
+	primary key (idLesao, idJogador),
+	foreign key (idLesao) references lesao (idLesao),
+	foreign key (idJogador) references jogador (idJogador));"""
+
+    cursor.execute(lesaojog)
+    connection.commit()
+
+    jogo = """create table if not exists jogo (
+	idJogo int unsigned not null,
+	idClube1 int unsigned not null,
+	idClube2 int unsigned not null,
+	idArbitro int unsigned not null,
+	resultado varchar(10),
+	idComp int unsigned not null,
+	primary key (idJogo),
+	foreign key (idClube1) references clube (idClube),
+	foreign key (idClube2) references clube (idClube),
+	foreign key (idArbitro) references arbitro (idArbitro),
+	foreign key (idComp) references competicao (idComp));"""
+
+    cursor.execute(jogo)
+    connection.commit()
+
+    cartaojog = """create table if not exists CartaoJogador (
+	idCartao int unsigned auto_increment,
+	idJogador int unsigned not null,
+	idJogo int unsigned not null,
+	idArbitro int unsigned not null,
+	cor varchar(20) not null,
+	primary key (idCartao),
+	foreign key (idJogador) references jogador (idJogador),
+	foreign key (idJogo) references jogo (idJogo),
+	foreign key (idArbitro) references arbitro (idArbitro));"""
+
+    cursor.execute(cartaojog)
+    connection.commit()
+
+    cartaotec = """create table if not exists CartaoTecnico (
+	idCartao int unsigned auto_increment,
+	idTecnico int unsigned not null,
+	idJogo int unsigned not null,
+	idArbitro int unsigned not null,
+	cor varchar(20) not null,
+	primary key (idCartao),
+	foreign key (idTecnico) references Tecnico (idTecnico),
+	foreign key (idJogo) references jogo (idJogo),
+	foreign key (idArbitro) references arbitro (idArbitro));"""
+
+    cursor.execute(cartaotec)
+    connection.commit()
+
+    titulo = """create table if not exists titulo (
+	idTitulo int unsigned auto_increment,
+	idComp int unsigned not null,
+	idClube int unsigned not null,
+	ano int not null,
+	idArtilheiro int unsigned not null,
+	primary key (idTitulo),
+	foreign key (idComp) references competicao (idComp),
+	foreign key (idClube) references clube (idClube),
+	foreign key (idArtilheiro) references jogador (idJogador));"""
+
+    cursor.execute(titulo)
+    connection.commit()
 
 
-def mainMenu():
+def menuPrincipal():
+
     while True:
-        print("----------------Menu Principal-----------------")
+        print('-------------------------Comandos-------------------------')
         print("1.- Create")
         print("2.- Read")
         print("3.- Update")
         print("4.- Delete")
         print("5.- Exit")
-        print("-----------------------------------------------")
+        print("----------------------------------------------------------")
 
-        print("Select one option: ")
-        option = int(input())
-        if not (1 <= option <= 5):
-            print("Incorrect option, insert again ...")
-        elif option == 5:
-            print("Thank you for using me!")
+        print("Digite sua opção: ")
+        opcao = int(input())
+        if 5 < opcao or opcao < 1:
+            print("Digite um valor válido!")
+        elif opcao == 5:
+            print("Obrigado por me usar!")
             break
-        else:
-            if option == 1:
-                createHandler()
-            elif option == 2:
-                readHandler()
-            elif option == 3:
-                updateHandler()
-            else:
-                deleteHandler()
-
-
-def createHandler():
-    pass
-
-
-def readHandler():
-    pass
-
-
-def updateHandler():
-    pass
-
-
-def deleteHandler():
-    pass
+        elif opcao == 1:
+            pass
+        elif opcao == 2:
+            pass
+        elif opcao == 3:
+            pass
+        elif opcao == 4:
+            pass
+        input("Pressione enter para voltar!")
 
 
 if __name__ == '__main__':
@@ -195,7 +193,8 @@ if __name__ == '__main__':
     cursor = connection.cursor()
 
     createTables()
-    mainMenu()
+
+    menuPrincipal()
 
     cursor.close()
     connection.close()
